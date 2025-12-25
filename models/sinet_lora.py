14ef05058a27fe59b88a7ea66e71867523871b4f
Nguyễn Quốc Hiệu <121285650+nqh-tq-32052503@users.noreply.github.com>
2025-12-25 17:19:35 +0700

    Fix bug

--- models/sinet_lora.py
+++ models/sinet_lora.py
@@ -41,7 +41,7 @@ class ViT_lora_co(VisionTransformer):
 def _create_vision_transformer(variant, pretrained=False, **kwargs):
     from functools import partial
     model = ViT_lora_co(num_classes=0, patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+        norm_layer=partial(nn.LayerNorm, eps=1e-6))
 
     if pretrained:
         import timm
